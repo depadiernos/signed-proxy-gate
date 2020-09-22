6f3de09cf890946a831c7204c8f78acfe8b06cd6
Dennis Padiernos <depadiernos@gmail.com>
2020-09-22 12:10:43 -0500

    create html/js to test signed proxy rules.

---
+++ index.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+document.querySelector("#valid").addEventListener("click", function(){getValid()})
+document.querySelector("#invalid").addEventListener("click", function(){getInvalid()})
+
+const token = jwt.sign({ foo: 'bar' }, 'secret', { algorithm: 'HS256' })
+export const getValid = () => {
+  console.log('click')
+  get(token)
+}
+export const getInvalid = () => {
+  get('')
+}
+const get = token => {
+  fetch('/gated-content', {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
